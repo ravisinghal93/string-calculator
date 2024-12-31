6e9c8ebb2c42cc96d6fec5d4e636ae0e9a2a78ca
ravisinghal93 <rahulgarg4578@gmail.com>
2024-12-31 17:08:44 +0530

    test cases for comma saprated values and empity string and it's funcnal level code

--- src/app/services/string-calculator.service.spec.ts
+++ src/app/services/string-calculator.service.spec.ts
@@ -13,4 +13,25 @@ describe('StringCalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('StringCalculatorService should return 0 for an empty string', () => {
+    const service = new StringCalculatorService();
+    expect(service.add("")).toEqual(0);
+  });
+
+  it('should return the number itself for a single number', () => {
+    expect(service.add("1")).toEqual(1);
+    expect(service.add("5")).toEqual(5);
+  });
+
+  it('should return the sum of two numbers', () => {
+    expect(service.add("1,2")).toEqual(3);    
+    expect(service.add("10,20")).toEqual(30);
+  });
+
+  it('should return the sum of three numbers', () => {
+    expect(service.add("1,2,3")).toEqual(6);
+    expect(service.add("10,20,30")).toEqual(60);
+  });  
+  
 });
--- src/app/services/string-calculator.service.ts
+++ src/app/services/string-calculator.service.ts
@@ -5,5 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class StringCalculatorService {
 
+  add(stringVal: string): number{
+    if (!stringVal) return 0;
+    let delimiter = /,|\n/;
+    let numbers = stringVal.split(delimiter).map(Number);
+    if (numbers.length > 0) {
+      return numbers.reduce((acc, curr) => {
+        return Number(acc + curr);
+      }, 0);
+    }
+    return 0;
+  }
+
   constructor() { }
 }
